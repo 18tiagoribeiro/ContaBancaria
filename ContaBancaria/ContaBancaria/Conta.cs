6508fdbb1b1fce9e2e91c190fe487ebf6afd5314
18tiagoribeiro <153301256+18tiagoribeiro@users.noreply.github.com>
2024-10-23 20:41:45 -0300

    Add files via upload

---
+++ ContaBancaria/ContaBancaria/Conta.cs
@@ -0,0 +1,16 @@
+﻿class Conta
+{
+    public string titular;
+    public int numero;
+    public int saldo;
+    public int senha;
+
+    public void ExibirConta()
+    {
+        Console.WriteLine($"Titular da conta: {titular}");
+        Console.WriteLine($"Número da conta: {numero}");
+        Console.WriteLine($"Saldo: {saldo} R$");
+        Console.WriteLine($"Senha: {senha}");
+        
+    }
+}
---
+++ ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,16 @@
+﻿Conta conta1 = new Conta();
+conta1.titular = "Tiago Ribeiro";
+conta1.numero = 25554501;
+conta1.saldo = 165780000;
+conta1.senha = 5667;
+
+Conta conta2 = new Conta();
+conta2.titular = "Edu";
+conta2.numero = 164156;
+conta2.saldo = 800000;
+conta2.senha = 5789;
+
+conta2.ExibirConta();
+Console.WriteLine("");
+conta1.ExibirConta();
+//Console.ReadLine();
